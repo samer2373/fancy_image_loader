b210106b75ff91607d918afacd818936692c4c98
Samer Abu-Sleem <56400978+samer2373@users.noreply.github.com>
2022-12-13 18:43:03 +0300

    format

--- lib/fancy_image_loader_method_channel.dart
+++ lib/fancy_image_loader_method_channel.dart
@@ -11,7 +11,8 @@ class MethodChannelFancyImageLoader extends FancyImageLoaderPlatform {
 
   @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
     return version;
   }
 }
